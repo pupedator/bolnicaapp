b9877e22b3c4df281c55d28f3d808066d3cd934c
Kamal Mahmud <144418724+pupedator@users.noreply.github.com>
2026-02-17 23:19:39 +0800

    Update copyright holders in LICENSE file

--- Bolnicaapp/Bolnicaapp/AppTheme.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-extension Color {
-    static let appBlue = Color(red: 41/255, green: 98/255, blue: 255/255)
-    static let appBlueDark = Color(red: 21/255, green: 101/255, blue: 192/255)
-    static let cardBackground = Color(.secondarySystemGroupedBackground)
-    static let sectionBackground = Color(.systemGroupedBackground)
-    static let badgeBackground = Color(.tertiarySystemFill)
-}
---
+++ README.md
@@ -0,0 +1,1 @@
+# bolnicaapp
